6621ac82edf6a9de50e3a9cc7f752a083a2977fb
Le A <112096529+achamberland11@users.noreply.github.com>
2025-03-06 03:12:26 -0500

    Implémentation projectiles + pointeur lazer

--- GameJam2025_01/Assets/Game/Scipts/TankCannon/TankCannon.cs
+++ GameJam2025_01/Assets/Game/Scipts/TankCannon/TankCannon.cs
@@ -43,12 +43,7 @@ public class TankCannon : MonoBehaviour
 
     public bool isCooldownG = false;
 
-    private GameObject r;
-    private Vector3 posR;
     private GameObject c;
-    private Vector3 posC;
-    private GameObject m;
-    private Vector3 posM;
 
 
     private void Awake()
@@ -65,8 +60,6 @@ public class TankCannon : MonoBehaviour
                 GameObject b = Instantiate(_bulletC, cCannonEnd.transform.position, cCannonEnd.transform.rotation);
                 b.GetComponent<Rigidbody>().AddForce(cCannonEnd.transform.TransformDirection(Vector3.forward) * 25f, ForceMode.Impulse);
                 isCooldownC = true;
-                Renderer rendererC = c.GetComponent<Renderer>();
-                rendererC.material.color = UnityEngine.Color.red;
                 if (isCooldownC == true)
                 {
                     StartCoroutine(CoolDownCannon());
@@ -84,8 +77,6 @@ public class TankCannon : MonoBehaviour
                 if (ammoM <= 0)
                 {
                     isCooldownM = true;
-                    Renderer rendererM = m.GetComponent<Renderer>();
-                    rendererM.material.color = UnityEngine.Color.red;
                 }
                 if (isCooldownM == true)
                 {
@@ -99,8 +90,6 @@ public class TankCannon : MonoBehaviour
                 GameObject g = Instantiate(_bulletG, gCannonEnd.transform.position, gCannonEnd.transform.rotation);
                 g.GetComponent<Rigidbody>().AddForce(gCannonEnd.transform.TransformDirection(Vector3.forward) * 25f, ForceMode.Impulse);
                 isCooldownG = true;
-                Renderer renderer = r.GetComponent<Renderer>();
-                renderer.material.color = UnityEngine.Color.red;
                 if (isCooldownG == true)
                 {
                     StartCoroutine(CoolDownGrenade());
@@ -143,8 +132,6 @@ public class TankCannon : MonoBehaviour
     {
         yield return new WaitForSeconds(cooldownTimeC);
         isCooldownC = false;
-        Renderer rendererC = c.GetComponent<Renderer>();
-        rendererC.material.color = UnityEngine.Color.green;
     }
 
     public IEnumerator CoolDownMachineGun()
@@ -152,16 +139,12 @@ public class TankCannon : MonoBehaviour
         yield return new WaitForSeconds(cooldownTimeM);
         ammoM = 50;
         isCooldownM = false;
-        Renderer rendererM = m.GetComponent<Renderer>();
-        rendererM.material.color = UnityEngine.Color.green;
     }
 
     public IEnumerator CoolDownGrenade()
     {
         yield return new WaitForSeconds(cooldownTimeG);
         isCooldownG = false;
-        Renderer renderer = r.GetComponent<Renderer>();
-        renderer.material.color = UnityEngine.Color.green;
     }
 
 }
